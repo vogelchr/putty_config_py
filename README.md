d0f4b35d8ec4a582f6d31e3aeb993758f64ca58b
Christian Vogel <vogelchr@vogel.cx>
2012-10-10 09:53:59 +0200

    Initial version.

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ putty_config_py
 ===============
 
 Short command-line utility to handle putty configuration (stored in the windows registry).
+
+My main use-case is to programmatically change the font/size for all
+putty sessions that are configured.
+
+Usage:
+
+	C:\Users\cvogel\Programs>python putty_config.py --list
+	6 session(s) in total.
+		com1_19200
+		com7_115200
+		Default%20Settings
+		linux
+		sc-linux-20120606
+		tort
+
+	C:\Users\cvogel\Programs>python putty_config.py --list "li*"
+	6 session(s) in total, 1 match filter "li*".
+		linux
+
+	C:\Users\cvogel\Programs>python putty_config.py --get Font "com*"
+	6 session(s) in total, 2 match filter "com*".
+
+	Session              Parameter Font
+	--------------------:----------------------------------------
+	com1_19200           Source Code Pro
+	com7_115200          Source Code Pro
+
+	C:\Users\cvogel\Programs>python putty_config.py --set Font=Courier "com*"
+	6 session(s) in total, 2 match filter "com*".
+	Updating parameter 'Font' to value 'Courier'.
+		com1_19200          : ok
+		com7_115200         : ok
+	No command given. Maybe you should try --help?
+
---
+++ putty_config.py
@@ -0,0 +1,112 @@
+#!python
+
+from _winreg import OpenKey, QueryInfoKey, EnumKey, EnumValue, QueryValueEx, \
+	SetValueEx, HKEY_CURRENT_USER,KEY_ALL_ACCESS,KEY_READ
+from optparse import OptionParser, OptionGroup
+import fnmatch
+import sys
+
+def registry_subkeys(key) :
+	ret = list()
+	subkeys, subvals, mtime = QueryInfoKey(key)
+	for i in range(subkeys) :
+		name = EnumKey(key,i)
+		ret.append(name)
+	return ret
+
+def registry_values(key) :
+	ret = dict()
+	subkeys, subvals, mtime = QueryInfoKey(key)
+	for i in range(subvals) :
+		name,data,type = EnumValue(key,i)
+		ret[name]=data
+	return ret
+
+class PuttyConfig :
+	def __init__(self,parent = None) :
+		if parent :
+			self.r_putty = parent
+		else :
+			self.r_putty = OpenKey(HKEY_CURRENT_USER,
+				r'Software\SimonTatham\PuTTY',0,KEY_READ)
+
+	def sessions(self) :
+		sess = OpenKey(self.r_putty,'Sessions',0,KEY_READ)
+		return registry_subkeys(sess)
+
+	def get_config(self,name) :
+		sess = OpenKey(self.r_putty,r'Sessions\%s'%(name),
+			0,KEY_READ)
+		return registry_values(sess)
+
+	def set_config_value(self,sess_name,par_name,par_value) :
+		sess = OpenKey(self.r_putty,r'Sessions\%s'%(sess_name),
+			0,KEY_ALL_ACCESS)
+		try :
+			rv_val, rv_type = QueryValueEx(sess,par_name)
+#			print 'Value: %s, %s, %s, python type %s'%(
+#				par_value,rv_val,rv_type,type(rv_val))
+			# try to encode the given value as the returned
+			# python type
+			new_val = type(rv_val)(par_value)
+			SetValueEx(sess,par_name,0,rv_type,new_val)
+		except WindowsError,e :
+			if e.winerror == 2 :
+				return False # Key not found.
+			print 'Windows Error: ',e
+			return False
+		return True
+
+if __name__ == '__main__' :
+	parser = OptionParser(usage='%prog [options] [Session]')
+	pgrp = OptionGroup(parser,'Commands')
+	pgrp.add_option('-l','--list',action='store_true',
+		dest='list',default=False,help='List sessions.')
+	pgrp.add_option('-g','--get',action='store',type='string',
+		dest='get',default=False,help='Get parameter NAME',
+		metavar='NAME');
+	pgrp.add_option('-s','--set',action='store',type='string',
+		dest='set',default=False,help='Set parameter NAME to VALUE',
+		metavar='NAME=VALUE');
+	parser.add_option_group(pgrp)
+			
+	opts,args = parser.parse_args()
+	PC = PuttyConfig()
+
+	# get all sessions, apply (optinal) filter.
+	sessions = PC.sessions()
+	if len(args) >= 1 :
+		filtsess = [s for s in sessions if fnmatch.fnmatch(s,args[0])]
+		print '%d session(s) in total, %d match filter \"%s\".'%(
+			len(sessions),len(filtsess),args[0])
+		sessions = filtsess
+	else :
+		print '%d session(s) in total.'%(len(sessions))
+	
+	if opts.list :
+		for s in sessions :
+			print '\t%s'%(s)
+		sys.exit(0)
+
+	if opts.get :
+		print ''
+		print 'Session              Parameter %s'%(opts.get)
+		print '--------------------:----------------------------------------'
+		for s in sessions :
+			cfg = PC.get_config(s)
+			val = cfg.get(opts.get,'-')
+			print '%-20s %s'%(s,val)
+		sys.exit(0)
+
+	if opts.set :
+		name,val = opts.set.split('=',1)
+		print 'Updating parameter \'%s\' to value \'%s\'.'%(name,val)
+		for s in sessions :
+			res = PC.set_config_value(s,name,val)
+			if res :
+				print '\t%-20s: ok'%(s)
+			else :
+				print '\t%-20s: not updated'%(s)
+		sys.exit(0)
+
+	print 'No command given. Maybe you should try --help?'
